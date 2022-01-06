8d0b262fa4c847a42ab036770890cd61c4aca4e8
mauriciojdk <mauriciojdk@gmail.com>
2022-01-05 23:04:49 -0300

    Adicionando class banco e getters e setters

---
+++ src/main/java/JavaBank/Banco.java
@@ -0,0 +1,24 @@
+package JavaBank;
+
+public class Banco {
+    private int id;
+    private String nome;
+
+    public Banco(){}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
