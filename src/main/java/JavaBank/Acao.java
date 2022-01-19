38d6dcfccc32604aec20a1db55845230a8436367
mauriciojdk <mauriciojdk@gmail.com>
2022-01-18 22:12:36 -0300

    Adicionando metodo de getPessoas utilizando SELECT

--- src/main/java/JavaBank/Acao.java
+++ src/main/java/JavaBank/Acao.java
@@ -1,73 +1,152 @@
 package JavaBank;
 
 import java.sql.*;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class Acao {
     private Connection connection = null;
     private PreparedStatement statement = null;
 
-    public Acao(){
+    public Acao() {
         connection = new ConnectionFactory().getConnection();
     }
 
-    public void cadastrarPessoa(Pessoa pessoa, Conta conta) throws SQLException {
-        String query = "INSERT INTO pessoa (cpf, data_nascimento, nome) VALUES (?, ?, ?)";
-        statement = connection.prepareStatement(query);
+    public void cadastrarPessoa(Pessoa pessoa, Conta conta) {
+        try {
 
-        statement.setString(1, pessoa.getCpf());
-        statement.setDate(2, Date.valueOf(pessoa.getDataNascimento()));
-        statement.setString(3, pessoa.getNome());
+            String query = "INSERT INTO pessoa (cpf, data_nascimento, nome) VALUES (?, ?, ?)";
+            statement = connection.prepareStatement(query);
 
-        statement.execute();
+            statement.setString(1, pessoa.getCpf());
+            statement.setDate(2, Date.valueOf(pessoa.getDataNascimento()));
+            statement.setString(3, pessoa.getNome());
+
+            statement.execute();
 
 
-        query = "INSERT INTO conta (numero_conta, pessoa_id) VALUES (?, ?)";
-        statement = connection.prepareStatement(query);
+            query = "INSERT INTO conta (numero_conta, pessoa_id) VALUES (?, ?)";
+            statement = connection.prepareStatement(query);
 
-        Integer last = pessoa.getLastId();
-        statement.setInt(1, conta.getNumConta());
-        statement.setInt(2, last);
+            Integer last = pessoa.getLastId();
+            statement.setInt(1, conta.getNumConta());
+            statement.setInt(2, last);
 
-        statement.execute();
-        statement.close();
+            statement.execute();
 
+            if (connection != null) {
+                connection.close();
+            }
+            if (statement != null) {
+                statement.close();
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Erro SQL " + e);
+        }
     }
 
-    public void removerConta(Integer id) throws SQLException {
-        String query = "DELETE FROM conta WHERE id_conta = ?";
-        PreparedStatement stm = null;
+    public void removerConta(Integer id) {
+        try {
 
-        stm = connection.prepareStatement(query);
-        stm.setInt(1, id);
-        stm.execute();
+            String query = "DELETE FROM conta WHERE id_conta = ?";
+            PreparedStatement stm = null;
 
-        query = "DELETE FROM pessoa WHERE id_pessoa = ?";
-        stm = connection.prepareStatement(query);
-        stm.setInt(1, id);
-        stm.execute();
+            stm = connection.prepareStatement(query);
+            stm.setInt(1, id);
 
-        if (connection != null){
-            connection.close();
-        } if (statement != null){
-            statement.close();
+            stm.execute();
+
+            query = "DELETE FROM pessoa WHERE id_pessoa = ?";
+            stm = connection.prepareStatement(query);
+            stm.setInt(1, id);
+
+            stm.execute();
+
+            if (connection != null) {
+                connection.close();
+            }
+            if (statement != null) {
+                statement.close();
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Erro SQL " + e);
         }
 
+    }
+
+    public void updateBank(Pessoa pessoa) {
+        try {
+
+            String query = "UPDATE pessoa SET cpf = ?, data_nascimento = ?, nome = ? WHERE id_pessoa = ?";
+            PreparedStatement stm = null;
+
+            stm = connection.prepareStatement(query);
+            stm.setString(1, pessoa.getCpf());
+            stm.setDate(2, Date.valueOf(pessoa.getDataNascimento()));
+            stm.setString(3, pessoa.getNome());
+            stm.setInt(4, pessoa.getLastId());
+
+            stm.execute();
+
+            if (connection != null) {
+                connection.close();
+            }
+            if (statement != null) {
+                statement.close();
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Erro SQL " + e);
+        }
 
     }
 
-    public void updateBank(Conta conta, Pessoa pessoa ) throws SQLException {
-        String query = "UPDATE pessoa SET cpf = ?, data_nascimento = ?, nome = ? WHERE id_pessoa = ?";
-        PreparedStatement stm = null;
 
-        stm = connection.prepareStatement(query);
-        stm.setString(1, pessoa.getCpf());
-        stm.setDate(2, Date.valueOf(pessoa.getDataNascimento()));
-        stm.setString(3, pessoa.getNome());
-        stm.setInt(4, pessoa.getLastId());
+    public List<Pessoa> getPessoas() {
+        List<Pessoa> pessoas = null;
+        try {
 
-        stm.execute();
+            String query = "SELECT * FROM pessoa";
+            pessoas = new ArrayList<Pessoa>();
+            PreparedStatement stm = null;
+            ResultSet rset = null;
 
+            stm = connection.prepareStatement(query);
+
+            rset = stm.executeQuery();
+
+            while (rset.next()) {
+                Pessoa pessoa = new Pessoa();
+
+                pessoa.setId(rset.getInt("id_pessoa"));
+                pessoa.setNome(rset.getString("nome"));
+                pessoa.setCpf(rset.getString("cpf"));
+                pessoa.setDataNascimento(LocalDate.parse((String.valueOf(rset.getDate("data_nascimento")))));
+
+                pessoas.add(pessoa);
+
+            }
+
+            if (connection != null) {
+                connection.close();
+            }
+            if (statement != null) {
+                statement.close();
+            }
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Erro SQL " + e);
+        }
+        return pessoas;
     }
-
 }
--- src/main/java/JavaBank/Test.java
+++ src/main/java/JavaBank/Test.java
@@ -1,18 +1,29 @@
 package JavaBank;
 
 import java.sql.SQLException;
+import java.text.ParseException;
 import java.time.LocalDate;
 
 public class Test {
 
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) throws SQLException, ParseException {
        //CREATE
-        LocalDate localDateAntigo = LocalDate.of(2003, 5, 05);
-        Pessoa pessoa = new Pessoa("08466356545",localDateAntigo ,"Mauricio");
+/*        LocalDate localDateAntigo = LocalDate.of(2003, 5, 05);
+        Pessoa pessoa = new Pessoa("000000",localDateAntigo ,"formato");
         Conta conta = new Conta();
         Acao acao = new Acao();
-        acao.cadastrarPessoa(pessoa, conta);
+        acao.cadastrarPessoa(pessoa, conta);*/
 
+
+        //SELECT
+        Acao acao = new Acao();
+        for (Pessoa p : acao.getPessoas() ){
+            System.out.println("ID: " + p.getId());
+            System.out.println("Pessoa: " + p.getNome());
+            System.out.println("CPF: " + p.getCpf());
+            System.out.println("Data de nascimento: " + p.getDataNascimento());
+            System.out.println("");
+        }
 
         //REMOVE
        // Acao acao = new Acao();
@@ -20,11 +31,11 @@ public class Test {
 
 
         //UPDATE
-        localDateAntigo = LocalDate.of(2022, 6, 06);
+        /*localDateAntigo = LocalDate.of(2022, 6, 06);
         pessoa.setCpf("123456");
         pessoa.setNome("LINDO");
         pessoa.setDataNascimento(localDateAntigo);
-        acao.updateBank(conta, pessoa);
+        acao.updateBank(pessoa);*/
 
 
         //PEGAR ULTIMO ID
